175bf090307d77edf8e89b071d53672cdb30b8a5
RENJITHPS10 <renjithpstech@gmail.com>
2024-12-08 16:51:11 +0530

    final commit

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'https://employeejsonserver-eyf8.onrender.com';
 
   constructor(private http: HttpClient) {}
 
